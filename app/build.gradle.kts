c39f74fd825af5c8afad9e133bdeb560d06d6ea8
vdema <demaisip.v@northeastern.edu>
2026-06-10 14:18:10 -0400

    Added necessary dependencies for project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    //id("com.google.gms.google-services") <-- Uncomment when implementing actual chat features
 }
 
 android {
@@ -54,4 +55,9 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation("androidx.navigation:navigation-compose:2.8.7")
+    implementation(libs.androidx.compose.material3)
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.10.2")
 }
